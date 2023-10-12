ee650365e04239f97c4314b29bd155ae2fc63fca
OmkarJadhav2020 <88571077+OmkarJadhav2020@users.noreply.github.com>
2023-10-12 15:17:51 +0530

    Create avg_sum.cpp

---
+++ avg_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int size;
+  cout<<"Enter no of elements to perform average of nos = ";
+  cin>>size;
+  int count = 0;
+  int arr[size];
+  for(int i = 0 ; i < size;i++)
+    {
+      cin>>arr[i];
+      count+=arr[i];
+    }
+  cout<<"Average of given no is = "<<count/size;
+  return 0;
+}
